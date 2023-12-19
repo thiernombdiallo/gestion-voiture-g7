06795ebe333bf7cecaf0586baeafd6dd49021cb8
Vevebel <diatta.veroniquemarie@gmail.com>
2023-12-19 09:12:47 +0000

    ceci est mon premier commit du jour

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import React from 'react';
+import Header from './components/Header'
 import MesVoitures from './components/MesVoitures';
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
+      <div>
+        <Header/>
+      </div>
+      
       <MesVoitures />
     </div>
   );
---
+++ src/components/CalculeAge.js
@@ -0,0 +1,14 @@
+
+// function CalculeAge(annee) {
+//     const anneeActuelle = new Date().getFullYear();
+//     const age = anneeActuelle - annee;
+//     return age;
+//   }
+  
+// CalculeAge.js
+export function calculerAgeVoiture(annee) {
+    const anneeActuelle = new Date().getFullYear();
+    const age = anneeActuelle - annee;
+    return age;
+  }
+  
